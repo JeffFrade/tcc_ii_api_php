79e2a0b9a953902a253f5eca62e42b4b067ea31b
Jefferson Frade <jefferrson.frade@gmail.com>
2023-03-30 20:04:33 -0300

    Refatorada as Providers

--- app/Core/Providers/RouteServiceProvider.php
+++ app/Core/Providers/RouteServiceProvider.php
@@ -1,11 +1,8 @@
 <?php
 
-namespace App\Providers;
+namespace App\Core\Providers;
 
-use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
--- config/app.php
+++ config/app.php
@@ -189,9 +189,9 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        App\Core\Providers\AppServiceProvider::class,
+        App\Core\Providers\AuthServiceProvider::class,
+        App\Core\Providers\RouteServiceProvider::class,
 
     ],
 
