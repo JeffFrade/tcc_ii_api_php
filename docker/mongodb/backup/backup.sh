fb7eb9c2008dcdee8cedada848b7c8611c561a5a
Jefferson Frade <jefferrson.frade@gmail.com>
2023-04-25 20:01:46 -0300

    Melhorias na rotina de backup

--- docker/mongodb/backup/backup.sh
+++ docker/mongodb/backup/backup.sh
@@ -3,10 +3,10 @@
 TIMESTAMP=$(date +"%F")
 DOCKER_BACKUP_DIR="/backup/$TIMESTAMP"
 
-docker exec -it tcc-ii-api-mongodb mongodump -u root -p @Tcc2023 -o $DOCKER_BACKUP_DIR
+docker exec -it tcc-ii-api-mongodb mongodump -u root -p !Tcc@2023 -o $DOCKER_BACKUP_DIR
 
 BACKUP_DIR="/home/ec2-user/backups/mongodb/$TIMESTAMP"
 
 mkdir -p $BACKUP_DIR
 
-sudo mv /home/ec2-user/tcc_ii-api/coletaEmissaoGases/docker/mongodb/backup/$TIMESTAMP/* $BACKUP_DIR
+sudo mv /home/ec2-user/tcc_ii_api_php/docker/mongodb/backup/$TIMESTAMP/* $BACKUP_DIR
--- docker/mongodb/backup/restore.sh
+++ docker/mongodb/backup/restore.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker exec -it tcc-ii-api-mongodb mongorestore -u=root -p=@Tcc2023 restore-db/
+docker exec -it tcc-ii-api-mongodb mongorestore -u=root -p=!Tcc@2023 restore-db/
